7ed2e4c9f75e81c7222ecdb9b039a954b1ca6aa2
Sudarshan Patil <mr.sudarsh@gmail.com>
2024-06-03 07:05:40 -0700

    new updates 1

--- src/components/Static.jsx
+++ src/components/Static.jsx
@@ -3,31 +3,31 @@ import React from 'react'
 function Static() {
   return (
     <>
-        <div className=" p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="stats" role="tabpanel" aria-labelledby="stats-tab">
-            <dl className="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-3 dark:text-white sm:p-8">
+        <div className=" p-4 bg-white rounded-lg md:p-8" id="stats" role="tabpanel" aria-labelledby="stats-tab">
+            <dl className="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto sm:grid-cols-3 xl:grid-cols-3 sm:p-8">
                 <div className="flex flex-col items-center justify-center">
                     <dt className="mb-2 text-2xl font-semibold">150+</dt>
-                    <dd className="text-gray-500 dark:text-gray-400 text-center">Web Surveys</dd>
+                    <dd className="text-gray-500 text-center">Web Surveys</dd>
                 </div>
                 <div className="flex flex-col items-center justify-center">
                     <dt className="mb-2 text-2xl font-semibold">48</dt>
-                    <dd className="text-gray-500 dark:text-gray-400 text-center">Leading Surveys</dd>
+                    <dd className="text-gray-500 text-center">Leading Surveys</dd>
                 </div>
                 <div className="flex flex-col items-center justify-center">
                     <dt className="mb-2 text-2xl font-semibold ">10+</dt>
-                    <dd className="text-gray-500 dark:text-gray-400 text-center">Web Projects <br/> &#40;Mini and Major&#41;</dd>
+                    <dd className="text-gray-500 text-center">Web Projects <br/> &#40;Mini and Major&#41;</dd>
                 </div>
                 <div className="flex flex-col items-center justify-center">
                     <dt className="mb-2 text-2xl font-semibold">25+</dt>
-                    <dd className="text-gray-500 dark:text-gray-400 text-center">Public Repository</dd>
+                    <dd className="text-gray-500 text-center">Public Repository</dd>
                 </div>
                 <div className="flex flex-col items-center justify-center">
                     <dt className="mb-2 text-2xl font-semibold">2</dt>
-                    <dd className="text-gray-500 dark:text-gray-400 text-center">Patents</dd>
+                    <dd className="text-gray-500 text-center">Patents</dd>
                 </div>
                 <div className="flex flex-col items-center justify-center">
                     <dt className="mb-2 text-2xl font-semibold">6</dt>
-                    <dd className="text-gray-500 dark:text-gray-400 text-center">IOT Projects</dd>
+                    <dd className="text-gray-500 text-center">IOT Projects</dd>
                 </div>
             </dl>
         </div>
