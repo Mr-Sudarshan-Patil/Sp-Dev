278a35f29ed93f3e418bf359e8b510731cb39399
Sudarshan Patil <mr.sudarsh@gmail.com>
2025-04-03 12:52:36 +0530

    update 3.0.0

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: "public",  
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "public/index.html", 
+    },
+  },
   base: "/Sp-Dev/",
 })
