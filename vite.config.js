53e1e3098611ffe0491867d65f995db0159b4db4
Sudarshan Patil <mr.sudarsh@gmail.com>
2025-04-03 13:41:28 +0530

    something added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
   publicDir: "public",  
   build: {
     outDir: "dist",
@@ -12,5 +11,5 @@ export default defineConfig({
       input: "public/index.html", 
     },
   },
-  // base: "/Sp-Dev/",
+  base: "/Sp-Dev/",
 })
